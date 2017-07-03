90c1de2ba504c3ac07596f35dfaf4698d419b841
inaesutil125 <inaesutil125@gmail.com>
2017-07-03 08:39:48 -0300

    atividade do dia 20/06

---
+++ JavaTDD/JavaBancoTestes/src/javabanco/entidade/ContaCorrente.java
@@ -0,0 +1,107 @@
+package javabanco.entidade;
+
+
+
+import java.util.ArrayList;
+
+
+
+
+
+
+
+public class ContaCorrente {
+
+	private int _numero;
+
+	private String _titular;
+
+	private float _saldo = 0;
+
+	private ArrayList<Float> _operacoes = new ArrayList<Float>();
+
+	
+
+	
+
+	
+
+	public ContaCorrente(int numero, String titular) {
+
+		_numero = numero;
+
+		_titular = titular;
+
+	}
+
+	public float saldo(){
+
+		return _saldo;
+
+	}
+
+	
+
+	public float credito(float valor) {
+
+		if (valor <= 0) throw new IllegalArgumentException("O valor da operacao deve ser maior que zero");
+
+		_saldo += valor;
+
+		_operacoes.add(valor);
+
+		return _saldo;
+
+	}
+
+	
+
+	public float debito(float valor) {
+
+		if (valor <= 0) throw new IllegalArgumentException("O valor da operacao deve ser maior que zero");
+
+		_saldo -= valor;
+
+		_operacoes.add(-valor);
+
+		return _saldo;
+
+	}
+
+	
+
+	public ArrayList<Float> extrato() {
+
+		return _operacoes;
+
+	}
+
+	
+
+	public int getNumero() {
+
+		return _numero;
+
+	}
+
+	
+
+	public String getTitular(){
+
+		return _titular;
+
+	}
+
+	
+
+	public float transferencia (float valor , ContaCorrente ccDestino){
+
+		this.debito(valor);
+
+		ccDestino.credito(valor);
+
+		return _saldo;
+
+	}
+
+}
---
+++ JavaTDD/JavaBancoTestes/src/javabanco/entidade/ContaCorrenteTest.java
@@ -0,0 +1,185 @@
+package javabanco.entidade;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertNotNull;
+
+
+
+import java.util.ArrayList;
+
+
+
+import org.junit.Test;
+
+
+
+public class ContaCorrenteTest {
+
+
+
+	@Test
+
+	public void testSaldo() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		assertEquals(0, cc.saldo(), 0);
+
+	}
+
+
+
+	@Test
+
+	public void testCredito() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		cc.credito(100);
+
+		assertEquals(100, cc.saldo(), 0);
+
+	}
+
+
+
+	@Test
+
+	public void testDebito() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		cc.debito(100);
+
+		assertEquals(-100, cc.saldo(), 0);
+
+	}
+
+
+
+	@Test
+
+	public void testExtrato() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		cc.credito(100);
+
+		cc.debito(100);
+
+		ArrayList<Float> op = cc.extrato();
+
+		assertEquals(2, op.size());
+
+		assertEquals(100, op.get(0), 0);
+
+		assertEquals(-100, op.get(1), 0);
+
+	}
+
+	
+
+	@Test
+
+	public void testSaldoPequenosFloats() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		cc.credito(0.1f);
+
+		cc.credito(0.2f);
+
+		assertEquals(0.3f, cc.saldo(), 0.0f);
+
+	}
+
+	
+
+	@Test(expected = IllegalArgumentException.class)
+
+	public void testCreditoValorNegativo() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		
+
+		cc.credito(-10);		
+
+	}
+
+	
+
+	@Test(expected = IllegalArgumentException.class)
+
+	public void testDebitoValorNegativo() {
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		
+
+		cc.debito(-10);		
+
+	}
+
+	
+
+	@Test
+
+	public void testConstrutor(){
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		assertNotNull(cc);
+
+	}
+
+	
+
+	@Test 
+
+	public void testGetTitular(){
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		assertEquals("joão da Silva", cc.getTitular());
+
+	}
+
+	
+
+	@Test 
+
+	public void testGetNumero(){
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		assertEquals(1234, cc.getNumero());
+
+	}
+
+	
+
+	@Test
+
+	public void testTrensferencia(){
+
+		ContaCorrente cc = new ContaCorrente(1234, "joão da Silva");
+
+		ContaCorrente ccDestino = new ContaCorrente (5678, "Gabriel Oliveira");
+
+		cc.debito(10);
+
+		ccDestino.credito(10);
+
+		assertEquals(-10, cc.saldo(), 0);
+
+		assertEquals(10, ccDestino.saldo(), 0);
+
+		
+
+	}	
+
+}
